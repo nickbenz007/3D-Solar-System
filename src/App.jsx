5a2891cd662b7b69ff20d551a82008a444e94a49
Nick Benz <nickbenz@Nicks-MacBook-Pro.local>
2025-01-08 14:35:31 -0800

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import { Perf } from 'r3f-perf';
+// import { Perf } from 'r3f-perf';
 import { MainContainer } from './MainContainer';
 // import { EffectComposer, Bloom } from '@react-three/postprocessing';
 
@@ -13,7 +13,7 @@ const App = () => {
         camera={{ fov: 110, position: [0, 3, 7], near: 0.5, far: 500 }}
         dpr={[1, 1.5]}
       >
-        <Perf openByDefault trackGPU={true} position="bottom-left" />
+        {/* <Perf openByDefault trackGPU={true} position="bottom-left" /> */}
         <OrbitControls />
         <MainContainer />
         {/* Post-Processing Effects */}
